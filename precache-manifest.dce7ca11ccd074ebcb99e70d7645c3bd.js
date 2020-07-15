9419cc395ea539207e208ef94747e4f895985bb0
Roaldkamman <55511890+Roaldkamman@users.noreply.github.com>
2020-07-15 14:42:07 -0400

    Updates

---
+++ precache-manifest.dce7ca11ccd074ebcb99e70d7645c3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f875088fe78880f7df429befcc2ba868",
+    "url": "/react_training_1/index.html"
+  },
+  {
+    "revision": "32dc269357b49000129d",
+    "url": "/react_training_1/static/css/main.00edb9b7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react_training_1/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_training_1/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32dc269357b49000129d",
+    "url": "/react_training_1/static/js/main.360fe1c4.chunk.js"
+  },
+  {
+    "revision": "9c037f5d4b76e20b80d2",
+    "url": "/react_training_1/static/js/runtime-main.b4a1362f.js"
+  }
+]);
